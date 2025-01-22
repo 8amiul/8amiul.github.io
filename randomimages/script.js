efe94b07ff669c83a88dede718429e327af85f92
8amiul <8amiul@gmail.com>
2025-01-22 20:10:35 +0600

    some fixes

--- randomimages/script.js
+++ randomimages/script.js
@@ -1,40 +1,46 @@
 const total_image = 62;
 const initial_position = Math.floor(Math.random() * total_image);
 let current_position = initial_position;
+const path = '/images/randomImages/';
 
-let img = document.querySelector(".randomimage-img");
 let description = document.querySelector(".randomimage-description");
 let image_position = document.querySelector('.img-position');
 let image_link = document.querySelector('#image-link');
 
-img.setAttribute('src', '/images/randomImages/' + images[current_position].img);
-description.innerText = images[current_position].description;
-image_position.innerText = '[' + (current_position+1) + '/' + total_image + ']';
-image_link.setAttribute('href', '/images/randomImages/' + images[current_position].img)
+function reload() {
+    const img = document.createElement('img');
+    img.setAttribute('src', path + images[current_position].img);
 
+    img.onload = function () {
+        img.setAttribute('class', 'randomimage-img');
+
+        description.innerText = images[current_position].description;
+        image_position.innerText = '[' + (current_position+1) + '/' + total_image + ']';
+        image_link.setAttribute('href', path + images[current_position].img);
+
+       image_link.appendChild(img);
+    }
+
+}
+reload();
 
 let previous = document.querySelector('.img-previous');
 let next = document.querySelector('.img-next');
 
 function prevFunc () {
     if (current_position > 0) {
+        image_link.removeChild(document.querySelector('.randomimage-img'));
         current_position--;
-        img.setAttribute('src', '/images/randomImages/' + current_position + '.jpg');
-        description.innerText = images[current_position].description;
-        image_position.innerText = '[' + (current_position+1) + '/' + total_image + ']';
-        image_link.setAttribute('href', '/images/randomImages/' + images[current_position].img)
+        reload();
     }
 };
 function nextFunc() {
     if (current_position < total_image-1) {
+        image_link.removeChild(document.querySelector('.randomimage-img'));
         current_position++;
-        img.setAttribute('src', '/images/randomImages/' + current_position + '.jpg');
-        description.innerText = images[current_position].description;
-        image_position.innerText = '[' + (current_position+1) + '/' + total_image + ']';
-        image_link.setAttribute('href', '/images/randomImages/' + images[current_position].img)
+        reload();
     }
 };
-
 
 next.addEventListener("click", nextFunc);
 previous.addEventListener("click", prevFunc);
