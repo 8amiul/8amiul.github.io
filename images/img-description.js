3156310bab0207a9238aacc0702731e0f8993aeb
8amiul <8amiul@gmail.com>
2025-06-27 11:11:30 +0600

    removed bloat

--- images/img-description.js
+++ images/img-description.js
@@ -7,81 +7,13 @@ let images = [
             so I was able to do most of the linux stuff there but the only limit was the CPU architecture!
         `
 	}, 
-
-	{ 
-		img: "1.jpg", 
-		description:
-        `
-            These are the albums that I've listened a lot of times on 2023, also 2024.
-        `
-	}, 
-
-	{ 
-		img: "2.jpg", 
-		description:
-        `
-            Collection of psychedelic songs.
-        `
-	}, 
-
-	{ 
-		img: "3.jpg", 
-		description: 
-        `
-            Many of these are from GTA VC radio, some I found from social media. Yeah, I like old-school stuff.
-        `
-	}, 
-
 	{ 
 		img: "4.jpg", 
 		description:
         `
             That's how my ArchLinux looked like when I started learning ricing. I really like the color scheme.
-
-            Do you like it?
         `
 	}, 
-
-
-	/*{ 
-		img: "6.jpg", 
-		description: "I don't know what it is! I clicked it when I was at my village."
-	}*/ 
-
-	/*{ 
-		img: "7.jpg", 
-		description: "It's the same thing as before."
-	},*/ 
-
-	{ 
-		img: "8.jpg", 
-		description:
-        `
-            Yeah, I like to listen music a lot. 
-            Why don't you visit my spotify profile and rate my music taste?
-            
-            I know my music taste sucks!
-        `
-	}, 
-
-
-	{ 
-		img: "11.jpg", 
-		description: 
-        `
-            red red red
-        `
-	}, 
-
-	{ 
-		img: "12.jpg", 
-		description:
-        `
-            They teach us that you can treat the derivative operator as a fraction but well then they say it isn't actually a fraction
-            Why just treat it like that? Let's learn the core.
-        `
-	}, 
-
 	{ 
 		img: "13.jpg", 
 		description:
@@ -90,104 +22,18 @@ let images = [
             Why do parents get happy when their children are happy?
         `
 	}, 
-
-	/*{ 
-		img: "14.jpg", 
-		description: "The same Termux chroot ArchLinux thing. I used to use arch, btw"
-	},*/ 
-
-	{ 
-		img: "15.jpg", 
-		description: "Collection of indie music from 2023"
-	}, 
 	{ 
 		img: "17.jpg", 
 		description: "I didn't know that there was dirt on those!"
 	}, 
-
 	{ 
 		img: "18.jpg", 
 		description: "A sleepy car."
 	}, 
-
-	{ 
-		img: "19.jpg", 
-		description: "Flowers, oh flowers."
-	}, 
-
-	{ 
-		img: "20.jpg", 
-		description: "July Revolution!"
-	}, 
-
-	{ 
-		img: "21.jpg", 
-		description: 
-        `
-            ঐ দেখা যায় তাল গাছ
-            ঐ আমাদের গাঁ। 
-            
-            There is no "তাল গাছ" anymore. There are just buildings and buildings
-        `
-	}, 
-	{ 
-		img: "23.jpg", 
-		description: "I don't remember why I kept it."
-	}, 
-
-	{ 
-		img: "24.jpg", 
-		description: "I was compiling Box64 for arm64 or arch64, idk. if you need it, you can find it in my github repo."
-	}, 
-
-	/*{ 
-		img: "25.jpg", 
-		description: `Dog.`
-	},*/ 
-
-	{ 
-		img: "26.jpg", 
-		description: "Who got the time to romanticize reading it?"
-	}, 
-
 	{ 
 		img: "27.jpg", 
 		description: "Buildings"
 	}, 
-	{ 
-		img: "29.jpg", 
-		description:
-        `
-		      Right now tablets are so useful and it will be more if Snapdragon processor gets more powerful day by day.
-        `
-	}, 
-
-	{ 
-		img: "31.jpg", 
-		description: "FUCK BIOLOGY! I art gibberish." 
-	}, 
-
-
-	{ 
-		img: "33.jpg", 
-		description: "A 7 year old cousin of mine drew this. Isn't it creepy? at the same time it's cool."
-	}, 
-
-	/*{ 
-		img: "34.jpg", 
-		description: "Sagol! oh, sagol."
-	},*/ 
-
-	{ 
-		img: "35.jpg", 
-		description:
-        `
-            TNT looks like a happy chemical compound to me. 
-            look, it has a methyl head and two hands and one leg.
-            It just says 'এক পায়ে খাড়া আমি।'
-        `
-	}, 
-
 	{ 
 		img: "36.jpg", 
 		description: "How clean!"
@@ -197,277 +43,54 @@ let images = [
 		img: "37.jpg", 
 		description: "Flora"
 	}, 
-
-	{ 
-		img: "38.jpg", 
-		description: "বিলাই"
-	}, 
-
-	/*{ 
-		img: "39.jpg", 
-		description: "First time got on these rides."
-	},*/ 
-
-	{ 
-		img: "40.jpg", 
-		description: "Nice"
-	}, 
-
-	/*{ 
-		img: "41.jpg", 
-		description: "I once for no reason was obsessed with CAS."
-	},*/ 
-
-	{ 
-		img: "42.jpg", 
-		description: "How does she even think of it?"
-	}, 
-
-	{ 
-		img: "43.jpg", 
-		description: 
-        `
-            Oh my! CasioFlex. A159W-N1 (left) which is mine and A168WA-1 (right) which is my uncle's.
-            I am exactly not sure of the model numbers.
-        `
-	}, 
-
-	{ 
-		img: "45.jpg", 
-		description:
-        `
-            Organic Chemistry. I got it from Udvash as they had a Chemistar event organized.
-        `
-	}, 
-
 	{ 
 		img: "46.jpg", 
-		description: "I visited Bangladesh Military Museum"
+		description: "I once visited Bangladesh Military Museum"
 	},
-
-
-
-    /*{ 
-        img: "48.jpg", 
-        description: "Not the best picture but I should keep some memories."
-    }, 
-
-    { 
-        img: "49.jpg", 
-        description: "BMARPC"
-    }, 
-
-    { 
-        img: "50.jpg", 
-        description: `I haven't enjoyed my college life like everyone has.`
-    },*/
-
     { 
         img: "51.jpg", 
         description: "There should be a blog regarding this picture on my site. My first meal of 2025."
     }, 
-
-    { 
-        img: "52.jpg", 
-        description: "White"
-    }, 
-
-    { 
-        img: "53.jpg", 
-        description: 
-        `
-            I just felt this yesterday. Mine was kind of same. I felt numb. I could move my hands but just in my dream maybe. Then suddenly I felt like something or a wave kind just pass over my body. Later I got my control again.
-        `
-    }, 
-
-    /*{ 
-        img: "54.jpg", 
-        description: "BRACU. They got a nice campus."
-    },*/ 
-
-
     { 
         img: "56.jpg", 
         description: "Stray cars."
     }, 
-
-    { 
-        img: "57.jpg", 
-        description: "I really like minimalism so I must like C."
-    }, 
-
     { 
         img: "58.jpg", 
-        description: "I guess I've got some mental issues. By the way, I don't feel this fear anymore."
+        description: "Sheesh!"
     }, 
-
     { 
         img: "59.jpg", 
         description:
         `
-            A retro vibe. By the way that music player was written by me and I am happy with it. Not but best but as a first project I think it's fine.
+            A retro vibe. I had written that music player when I first learned C.
         `
     }, 
-
-    { 
-        img: "60.jpg", 
-        description:
-        `
-            If authority had allowed them to operate this operation then perhaps the kid could have survived.
-            Also we have to make a lot of improvements in these type of forces otherwise we will pay for it.
-        `
-    }, 
-
     { 
         img: "61.jpg", 
         description: "Neofetch with uberzug ig."
     },
-
 	{
 		img: "62.jpg",
 		description: "I see this flower wherever I go."
 	},
-
 	{
 		img: "63.jpg",
-		description: "Kiddo"
+		description: "Found it on my uncle's desktop."
 	},
-	/*{
-		img: "64.jpg",
-		description: 
-		`
-			It's true! Whenever you will start looking for the returns, you 
-			will only feel that you don't get what you did for them.
-			I now understand giving and getting the same amount of efforts
-			or help in return isn't always possible. Helping someone, we should
-			not expect something in return. I know there are cases where this 
-			theory doesn't work but those cases shouldn't be called help, those
-			are trade. 
-		`
-	},*/
-	/*{
-		img: "65.jpg",
-		description: "This has a deep meaning. Why don't you read the caption once again?"
-	},*/
-	{ 
-		img: "66.jpg", 
-		description: "TermusJS, written in pure HTML, CSS and JS."
-	}, 
-
-	{ 
-		img: "67.jpg", 
-		description: "GTA VC Nextgen Edition is amazing. Vice city is fully ported in GTA 4's rage engine."
-	}, 
-
-	{ 
-		img: "68.jpg", 
-		description: "Ugh! I just couldn't complete GTA IV. Recently I lost the save file and all my progress has gone."
-	}, 
-
-	/*{ 
-		img: "69.jpg", 
-		description: "..."
-	},*/ 
-
-	{ 
-		img: "70.jpg", 
-		description: "I was awake on those nights and it was freaking chaotic XD."
-	}, 
-
 	{ 
 		img: "71.jpg", 
-		description: "I gave her a new hairstyle."
+		description: "New hairstyle."
 	}, 
-
-	/*{ 
-		img: "72.jpg", 
-		description: "I kept this head after beheading her. Now it's a nice showpiece"
-	},*/ 
-
-	/*{ 
-		img: "73.jpg", 
-		description: "She is Serena, a vampire. She can turn you vampire in Skyrim"
-	},*/ 
-
-	/*{ 
-		img: "74.jpg", 
-		description: "The death of the final boss Alduin."
-	},*/ 
-
-	{ 
-		img: "75.jpg", 
-		description: "I didn't think Skyrim would be that fun to play."
-	}, 
-
-	{ 
-		img: "76.jpg", 
-		description: "Aurora!"
-	}, 
-
-	/*{ 
-		img: "77.jpg", 
-		date: "5 Feb 2025",
-		description: "tf Tommy! It's a glitch lol. I think it's hilarious."
-	},*/
-	{
-		img: "80.jpg",
-		date: "23 Feb 2025",
-		description: "Every family reunion is so special. This time I met with so many people. Hope we can be always this much happy."
-	},
-	{
-		img: "81.jpg",
-		date: "23 Feb 2025",
-		description: "Artificial Flowers"
-	},
-	{
-		img: "82.jpg",
-		date: "23 Feb 2025",
-		description: "That ceremony deserved it."
-	},
 	{
 		img: "83.jpg",
 		date: "04 Mar 2025",
 		description: "Debian with Chicago95"
 	},
-	/*{
-		img: "84.jpg",
-		date: "09 Mar 2025",
-		description: "Smooth!"
-	},*/
-	/*{
-		img: "85.jpg",
-		date: "09 Mar 2025",
-		description: "That's me I guess"
-	},*/
-	/*{
-		img: "86.jpg",
-		date: "17 Mar 2025",
-		description: "F all kryptonites!"
-	},*/
-	/*{
-		img: "87.jpg",
-		date: "17 Mar 2025",
-		description: "I told ChatGPT to roast me and he cooked!"
-	},*/
-	{
-		img: "88.jpg",
-		date: "20 Mar 2025",
-		description: "Addicted to smol car videos"
-	},
-	{
-		img: "89.jpg",
-		date: "22 Mar 2025 | 12:45 AM",
-		description: "Lain"
-	},
 	{
 		img: "90.jpg",
 		date: "22 Mar 2025 | 08:28 PM",
 		description: "Every lie we tell incurs a debt to the truth \n- Valery Legasov"
-	},
-	{
-		img: "91.jpg",
-		date: "24 Mar 2025 | 01:30 AM",
-		description: "ক্ষুধার রাজ্যে পৃথিবী গদ্যময়, পূর্ণিমা চাঁদ যেন ঝলসানো রুটি \n- সুকান্ত ভট্টাচার্য"
 	},
 	{
 		img: "93.jpg",
@@ -480,28 +103,13 @@ let images = [
 		description: "From the river to sea, Palestine will be free!"
 	},
 	{
-		img: "ranaplazap.jpg",
-		date: "24 Apr 2025",
-		description: "12 years now"
-	},
-	{
 		img: "integralflower.jpg",
 		date: "30 Apr 2025 | 12:50 PM",
 		description: "Another freaking flower!"
 	},
-	{
-		img: "cat rumi.jpg",
-		date: "15 May 2025 | 10:12 PM",
-		description: ":D"
-	},
     {
         img: "idkman.png",
         date: "25 May 2025 | 6:06 PM",
-        description: "Another run."
-    },
-	{
-		img: "meme.png",
-		date: "14 June 2025 | 6:34 PM",
-		description: "I must mention one of my friends, some of my cousins, 3:00 AM dark room, celing fan, insomnia, my phone and the ancient desktop"
-	}
+        description: "Another Skyrim run."
+    }
 	]
